3dbc4d1d553f8a999f5094d1bbc1804bd5a76d27
fathanmuhandis <m.fathan11@ui.ac.id>
2022-12-10 17:43:30 +0700

    perbaiki sedikit

--- ProyekAkhir_tb.vhd
+++ ProyekAkhir_tb.vhd
@@ -10,37 +10,39 @@ architecture bench of ProyekAkhir_tb is
     component ProyekAkhir is
         port (
             -- Input
-            M : IN STD_LOGIC; --ON/OFF untuk menyalakan atau mematikan mesin
+            M   : IN STD_LOGIC; --ON/OFF untuk menyalakan atau mematikan mesin
             CLK : IN STD_LOGIC; -- CLOCK
             Sen : IN STD_LOGIC; -- Sensor
-            D : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- Arah
-            H : IN STD_LOGIC; -- Kail dengan jaring
+            D   : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- Arah
+            H   : IN STD_LOGIC; -- Kail dengan jaring
     
             -- Output
             Sampah  : OUT STD_LOGIC; -- Sampah
             Lamp    : OUT STD_LOGIC; -- Lampu Led untuk arah
-            Alarm    : OUT STD_LOGIC_VECTOR  -- Bel alarm untuk Kail
+            Alarm   : OUT STD_LOGIC -- Bel alarm untuk Kail
         );
     end component;
 
-    signal M, CLK, Sen, H, Sampah : STD_LOGIC;
-    signal D : STD_LOGIC_VECTOR(1 downto 0);
-    constant period : time      := 30 ps;
-    signal max_clk  : integer   := 60;
-    signal count    : integer   := 0;
-    signal sensor   : STD_LOGIC;
-    signal led      : STD_LOGIC;
-    signal buzz     : STD_LOGIC_VECTOR(1 downto 0);
-
+    signal Machine      : STD_LOGIC;
+    signal CLK          : STD_LOGIC;
+    signal Sensor       : STD_LOGIC;
+    signal Direction    : STD_LOGIC_VECTOR (1 downto 0);
+    signal Hook         : STD_LOGIC;
+    signal Sampah       : STD_LOGIC;
+    signal led          : STD_LOGIC;
+    signal buzz         : STD_LOGIC;
+    constant period     : time      := 20 ps;
+    signal max_clk      : integer   := 60;
+    signal count        : integer   := 0;
 
 begin
 
     testMesin : ProyekAkhir port map (
-        M => M,
+        M => Machine,
         CLK => CLK,
-        Sen => Sen,
-        D => D,
-        H => H,
+        Sen => Sensor,
+        D (1 downto 0) => Direction (1 downto 0),
+        H => Hook,
         Sampah => Sampah,
         Lamp => Led,
         Alarm => Buzz
@@ -49,57 +51,94 @@ begin
     process
         constant led_red      : STD_LOGIC := '0';
         constant led_green    : STD_LOGIC := '1';   
-        constant buzz_front      : STD_LOGIC_VECTOR := "00";
-        constant buzz_back      : STD_LOGIC_VECTOR := "01";
-        constant buzz_left      : STD_LOGIC_VECTOR := "10";
-        constant buzz_right      : STD_LOGIC_VECTOR := "11";
+        constant buzz_off     : STD_LOGIC := '0';
+        constant buzz_front   : STD_LOGIC := '1';
+        constant buzz_back    : STD_LOGIC := '1';
+        constant buzz_left    : STD_LOGIC := '1';
+        constant buzz_right   : STD_LOGIC := '1';
     begin
-        M <= '1';
-        Sen <= '0';
-        H <= '0';
+        Machine <= '0';
+        Sensor <= '0';
+        Hook <= '0';
         wait for period;
         assert (led = led_red)
-            report "LED is RED, Machine Ready!" severity note;
+            report "Machine OFF!" severity note;
+
+        Machine <= '1';
+        Sensor <= '0';
+        Hook <= '0';
+        wait for period;
+        assert (led = led_red)
+            report "Machine ON!" severity note;
         
-        D <= "00";
+        Machine <= '1';
+        Sensor <= '0';
+        Hook <= '0';
+        Direction <= "00";
         wait for period;
         assert (buzz = buzz_front)
             report "Hook to Forward" severity note;
 
-        D <= "01";
+        Machine <= '1';
+        Sensor <= '0';
+        Hook <= '0';
+        Direction <= "01";
         wait for period;
         assert (buzz = buzz_back)
             report "Hook to Back" severity note;
-            
-        D <= "10";
+
+        Machine <= '1';
+        Sensor <= '0';
+        Hook <= '0';
+        Direction <= "10";
         wait for period;
         assert (buzz = buzz_left)
             report "Hook to Left" severity note;
 
-        D <= "11";
+        Machine <= '1';
+        Sensor <= '0';
+        Hook <= '0';
+        Direction <= "11";
         wait for period;
         assert (buzz = buzz_right)
             report "Hook to Right" severity note;
-        
-        H <= '1';
+
+        Machine <= '1';
+        Sensor <= '0';
         wait for period;
         assert (led = led_red)
             report "Hook going down to find debris" severity note;
 
-        Sen <= '0';
+        Machine <= '1';
+        Sensor <= '0';
+        Hook <= '1';
         wait for period;
         assert (led = led_red)
             report "Picking up trash" severity note;
 
-        Sen <= '1';
+        Machine <= '1';
+        Sensor <= '1';
+        Hook <= '1';
+        Sampah <= '1';
         wait for period;
         assert (led = led_green)
             report "The sensor detects the trash is full" severity note;
 
-        H <= '0';
+        Machine <= '1';
+        Sensor <= '1';
+        Hook <= '0';
+        Sampah <= '1';
         wait for period;
         assert (led = led_green)
             report "Hook going up to pickup the debris" severity note;
+
+        Machine <= '0';
+        Sensor <= '0';
+        Hook <= '0';
+        Sampah <= '1';
+        wait for period;
+        assert (led = led_red)
+            report "Machine going OFF!" severity note;
         wait;
     end process;
 
